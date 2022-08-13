e96feca245947654d9281d8cf51d545602ed04f7
Chinna394 <102656995+Chinna394@users.noreply.github.com>
2022-08-13 22:57:51 +0530

    import java.util.Scanner; class Float {     public static void main(String args[])     {         double a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextDouble();         b=sc.nextDouble();         c=a*b;
            System.out.printf("%.2f",c);
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Float
+{
+    public static void main(String args[])
+    {
+        double a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextDouble();
+        b=sc.nextDouble();
+        c=a*b;
+        
+        System.out.printf("%.2f",c);
+    }
+}
