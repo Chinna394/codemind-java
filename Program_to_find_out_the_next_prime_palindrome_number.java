db763cc42954287be8eb91e6e398f21db98292f0
Chinna394 <102656995+Chinna394@users.noreply.github.com>
2022-10-11 12:45:43 +0530

    import java.util.Scanner; class Pri {     public static boolean isprime(int x)     {         int count=0;         for(int i=2;i<=(int)Math.sqrt(x);i++)         {             if(x%i==0)             {                 count++;             }         }         if(count==0)         {             return true;         }         else         {             return false;         }     }
        public static boolean ispall(int x)
        {
            int r,rev=0,t;
            t=x;
            while(x>0)
            {
                r=x%10;
                rev=rev*10+r;
                x=x/10;
            }
            if(t==rev)
            {
                return true;
            }
            else
            {
                return false;
            }
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n;
            n=sc.nextInt();
            for(int i=n+1;;i++)
            {
                if(isprime(i) && ispall(i))
                {
                    System.out.println(i+"");
                    break;
                }
            }
        }
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
-class Prime
-{   
-    public static boolean isprime(int a)
+class Pri
+{
+    public static boolean isprime(int x)
     {
-    int count=0;
-    for(int i=2;i<=(int)Math.sqrt(a);i++ )
-      {
-          if(a%i==0)
-           count++;
-      }
-      if(count==0)
-       {
-           return true;
-       }
-       else
+        int count=0;
+        for(int i=2;i<=(int)Math.sqrt(x);i++)
+        {
+            if(x%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==0)
+        {
+            return true;
+        }
+        else
         {
             return false;
         }
-    }   
-    public static boolean ispalindrome(int a)
-     {
-         int sum=0,r;
-          int temp=a;
-         while(a>0)
-         {
-             r=a%10;
-             sum=sum*10+r;
-             a=a/10;
-         }
-         if(temp==sum)
-         {
-             return true;
-         }
-         else
-         {
-             return false;
-         }
-     }
+    }
+    
+    public static boolean ispall(int x)
+    {
+        int r,rev=0,t;
+        t=x;
+        while(x>0)
+        {
+            r=x%10;
+            rev=rev*10+r;
+            x=x/10;
+        }
+        if(t==rev)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
@@ -44,13 +47,11 @@ class Prime
         n=sc.nextInt();
         for(int i=n+1;;i++)
         {
-            if(isprime(i) && ispalindrome(i))
-              {
-                  System.out.println(i);
-                   break;
-              }
+            if(isprime(i) && ispall(i))
+            {
+                System.out.println(i+"");
+                break;
+            }
         }
-        
-        
     }
 }
