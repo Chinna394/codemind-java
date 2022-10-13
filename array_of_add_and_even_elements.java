cb44d88fb94eb5e7e80ce6518f6bc56dc674da48
Chinna394 <102656995+Chinna394@users.noreply.github.com>
2022-10-14 01:37:36 +0530

    import java.util.Scanner; class even {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int j=0;j<n;j++)         {             if(x[j]%2!=0)             {                 System.out.print(x[j]+" ");             }         }         for(int j=0;j<n;j++)         {             if(x[j]%2==0)             {                 System.out.print(x[j]+" ");             }         }     } }

---
+++ array_of_add_and_even_elements.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class even
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(x[j]%2!=0)
+            {
+                System.out.print(x[j]+" ");
+            }
+        }
+        for(int j=0;j<n;j++)
+        {
+            if(x[j]%2==0)
+            {
+                System.out.print(x[j]+" ");
+            }
+        }
+    }
+}
