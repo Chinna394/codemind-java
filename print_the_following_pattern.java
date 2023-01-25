b379abce97495a8a6a357df1dd630864b726bb18
Chinna394 <102656995+Chinna394@users.noreply.github.com>
2023-01-25 16:05:42 +0530

    import java.util.Scanner; class code {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=0;j<n;j++)             {                 if(i==j)                 {                     System.out.print("0");                 }                 else                 {                     System.out.print("x");                 }             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
-class Code
+class code
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         int n;
         n=sc.nextInt();
-        for(int i=n;i>=1;i--)
+        for(int i=0;i<n;i++)
         {
-            for( int j=1;j<=i;j++)
+            for(int j=0;j<n;j++)
             {
-                System.out.print(j);
+                if(i==j)
+                {
+                    System.out.print("0");
+                }
+                else
+                {
+                    System.out.print("x");
+                }
             }
             System.out.println();
         }
